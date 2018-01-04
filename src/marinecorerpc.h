a1e7e433c6fd32492fc116ba8adb49c07a96bc70
Bruce Wayne <help@jarvis.help>
2018-01-04 14:06:53 +0300

    comments

--- src/marinecorerpc.h
+++ src/marinecorerpc.h
@@ -1,9 +1,3 @@
-// Copyright (c) 2010 Satoshi Nakamoto
-// Copyright (c) 2009-2012 The Marinecore developers
-// Copyright (c) 2011-2012 Litecoin Developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef _MARINECORERPC_H_
 #define _MARINECORERPC_H_ 1
 
@@ -20,20 +14,10 @@ json_spirit::Object JSONRPCError(int code, const std::string& message);
 void ThreadRPCServer(void* parg);
 int CommandLineRPC(int argc, char *argv[]);
 
-/** Convert parameter values for RPC call from strings to command-specific JSON objects. */
 json_spirit::Array RPCConvertValues(const std::string &strMethod, const std::vector<std::string> &strParams);
 
-/*
-  Type-check arguments; throws JSONRPCError if wrong type given. Does not check that
-  the right number of arguments are passed, just that any passed are the correct type.
-  Use like:  RPCTypeCheck(params, boost::assign::list_of(str_type)(int_type)(obj_type));
-*/
 void RPCTypeCheck(const json_spirit::Array& params,
                   const std::list<json_spirit::Value_type>& typesExpected);
-/*
-  Check for expected keys/value types in an Object.
-  Use like: RPCTypeCheck(object, boost::assign::map_list_of("name", str_type)("value", int_type));
-*/
 void RPCTypeCheck(const json_spirit::Object& o,
                   const std::map<std::string, json_spirit::Value_type>& typesExpected);
 
@@ -47,9 +31,6 @@ public:
     bool okSafeMode;
 };
 
-/**
- * Marinecore RPC command dispatcher.
- */
 class CRPCTable
 {
 private:
@@ -59,13 +40,6 @@ public:
     const CRPCCommand* operator[](std::string name) const;
     std::string help(std::string name) const;
 
-    /**
-     * Execute a method.
-     * @param method   Method to execute
-     * @param params   Array of arguments (JSON objects)
-     * @returns Result of the call.
-     * @throws an exception (json_spirit::Value) when an error happens.
-     */
     json_spirit::Value execute(const std::string &method, const json_spirit::Array &params) const;
 };
 
